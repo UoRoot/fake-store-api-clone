bd6d7e8833e5205ca88caff3e52f39e28bafc7a3
UoRoot <burnedprogrammer@gmail.com>
2025-01-09 17:11:32 -0500

    fix(api-controller): add correct method to eliminate a product

--- src/main/java/com/fakestoreapi/clone/api/controllers/v1/ProductController.java
+++ src/main/java/com/fakestoreapi/clone/api/controllers/v1/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,7 +60,7 @@ public class ProductController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<ProductResponse> delete(@PathVariable Long id) {
         Optional<Product> product = productService.deleteProduct(id);
 
